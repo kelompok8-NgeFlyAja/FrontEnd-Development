d3fe22c8338b08c762ce793db86d51bab1a0b19e
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-11-24 04:47:26 +0700

    Adding a homepage in components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Beranda from "../components/Beranda";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Beranda />
+    </div>
+  );
+};
+
+export default Home;
