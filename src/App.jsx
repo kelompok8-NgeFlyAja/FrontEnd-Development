b548e8e8eb87c0d1677395675c13abbd67abca42
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-11-24 05:08:04 +0700

    Modifying the code file in the App.jsx section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Beranda from "./components/Beranda";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/Beranda" element={<Beranda />} />
         <Route
           path="*"
