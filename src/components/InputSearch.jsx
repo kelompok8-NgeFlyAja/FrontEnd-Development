3f313c32cf9aef0349419a0fd1170093caf4a614
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-11-24 05:18:40 +0700

    Removing comments in the code

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -35,29 +35,3 @@ const InputSearch = ({ placeholder, value, onChange, onReset }) => {
   );
 };
 export default InputSearch;
-// import React, { useRef } from "react";
-// import searchIcon from "../../public/Input_Search_Icon.svg";
-// const InputSearch = ({ placeholder }) => {
-//   const searchRef = useRef();
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const value = searchRef.current.value;
-//   };
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div className="flex items-center relative">
-//         <input
-//           type="text"
-//           // placeholder="Cari di sini ..."
-//           placeholder={placeholder}
-//           className="px-6 py-4 bg-slate-200 rounded-3xl cursor-pointer w-full md:w-96"
-//           ref={searchRef}
-//         />
-//         <button className="absolute right-0 end-4 md:end-8">
-//           <img src={searchIcon} alt="input icon" width={24} height={24} />
-//         </button>
-//       </div>
-//     </form>
-//   );
-// };
-// export default InputSearch;
