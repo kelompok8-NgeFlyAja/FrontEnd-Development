60b43d4be34a9a0290f252c4c96f09bab644ea54
raihannajmi <54610465+raihannajmi@users.noreply.github.com>
2024-11-26 19:46:01 +0700

    fix: Homepage Featured Flight (#1)
    Co-authored-by: Najmi Raihan <najmiraihan@Najmis-MacBook-Pro.local>

--- src/components/FeautredSearch.jsx
+++ src/components/FeautredSearch.jsx
@@ -13,7 +13,15 @@ function FeaturedSearch() {
   const [selected, setSelected] = React.useState("Semua");
 
   return (
-    <div className="content max-w-[1098px] w-full mx-auto -mt-12 relative pt-6 bg-none rounded-lg">
+    <div
+      className="content max-w-[1098px] w-full mx-auto relative pt-6 bg-none rounded-lg mt-[36px]"
+      style={{ zIndex: 1 }} // Pastikan ini berada di atas elemen absolute sebelumnya jika diperlukan
+    >
+      {/* Destinasi Favorit */}
+      <div className="text-base font-bold leading-6 mb-4">
+        Destinasi Favorit
+      </div>
+      {/* Kategori */}
       <div className="flex gap-4 flex-wrap justify-start">
         {categories.map((category) => (
           <Button
@@ -22,7 +30,7 @@ function FeaturedSearch() {
             className={`flex gap-2 items-center px-6 py-4 rounded-xl ${
               selected === category
                 ? "bg-[#7126B5] text-white"
-                : "text-[#3C3C3C]"
+                : "bg-[#E2D4F0] text-[#3C3C3C]"
             }`}
             onClick={() => setSelected(category)}
           >
