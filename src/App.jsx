87d60ceae8682e8af06e7f907b5b3895662fd03e
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-11-27 15:16:50 +0700

    Adding an import for Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Search from "./pages/Search";
+import Checkout from "./components/Checkout";
 
 const router = createBrowserRouter([
   {
