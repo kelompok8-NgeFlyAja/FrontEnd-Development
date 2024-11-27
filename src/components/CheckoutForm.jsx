19eebc2411c818910f4db4dce0371a7ec780a472
Ferdi Firdaus Ega Pratama <ferdifirdaus157@gmail.com>
2024-11-27 15:12:34 +0700

    Adding a CheckoutForm.jsx file

---
+++ src/components/CheckoutForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const CheckoutForm = ({ children, title, isSaved }) => {
+  return (
+    <div>
+      <div className="bg-[#3C3C3C] text-white py-2.5 px-4 rounded-tr-2xl rounded-tl-2xl flex justify-between items-center ">
+        {title}
+        {isSaved && <img src={checklistIcon} width={24} height={24} />}
+      </div>
+      {children}
+    </div>
+  );
+};
+export default CheckoutForm;
