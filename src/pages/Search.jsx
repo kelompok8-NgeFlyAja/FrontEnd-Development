daf50d36f77c221f70083a6e5c157892a3cb39a4
AbdiPranawa <asaturaardana@gmail.com>
2024-11-27 19:36:13 +0700

    feat: add sort

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,12 @@
 import DateFilter from "@/components/DateFilter";
+import Sort from "@/components/Sort";
 import React from "react";
 
 export default function Search() {
   return (
     <div>
       <DateFilter />
+      <Sort />
     </div>
   );
 }
