4320bdc93f30d118aae318b21216e073c7723612
AbdiPranawa <asaturaardana@gmail.com>
2024-11-29 00:32:08 +0700

    feat: add ResultNotFound

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,6 +4,7 @@ import Sort from "@/components/Sort";
 import ChangeResult from "@/components/ChangeResult";
 import SoldOut from "@/components/SoldOut";
 import Filter from "@/components/Filter";
+import ResultNotFound from "@/components/ResultNotFound";
 import React from "react";
 
 export default function Search() {
@@ -14,7 +15,18 @@ export default function Search() {
       <DateFilter />
       <Sort />
       {/* <SoldOut /> */}
-      <Filter />
+
+      <div className="flex">
+        {/* Komponen Filter dengan lebar tetap */}
+        <div className="w-[500px] ">
+          <Filter />
+        </div>
+
+        {/* Komponen ResultNotFound di tengah */}
+        <div className="flex-1 mr-[300px]">
+          <ResultNotFound />
+        </div>
+      </div>
     </div>
   );
 }
