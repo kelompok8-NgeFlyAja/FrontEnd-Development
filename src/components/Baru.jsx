12dab54a1d6a2d8fde9b683f378a1d554f944f8a
raihannajmi <54610465+raihannajmi@users.noreply.github.com>
2024-12-02 21:55:08 +0700

    feat: example new component (#4)
    Co-authored-by: Najmi Raihan <najmiraihan@Najmis-MacBook-Pro.local>

---
+++ src/components/Baru.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Baru() {
+  return (
+    <div className="mx-auto">
+      <div className="flex-col">
+        <div></div>
+        <div className="flex-row">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Baru;
