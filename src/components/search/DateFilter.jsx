edb7d4c5546e82007a63b9c9acad964301173ea0
AbdiPranawa <asaturaardana@gmail.com>
2024-12-08 17:30:34 +0700

    fix: cleaning code DateFilter

--- src/components/search/DateFilter.jsx
+++ src/components/search/DateFilter.jsx
@@ -1,53 +1,24 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function DateFilter() {
-  const dummyDates = [
-    { day: "Selasa", date: "01/03/2023" },
-    { day: "Rabu", date: "02/03/2023" },
-    { day: "Kamis", date: "03/03/2023" },
-    { day: "Jumat", date: "04/03/2023" },
-    { day: "Sabtu", date: "05/03/2023" },
-    { day: "Minggu", date: "06/03/2023" },
-    { day: "Senin", date: "07/03/2023" },
-    { day: "Selasa", date: "08/03/2023" },
-  ];
+const DateFilter = ({ day, date, onClick, isSelected }) => {
+  const [hovered, setHovered] = useState(false);
 
-  const [selectedDate, setSelectedDate] = useState(dummyDates[1].date);
+  const handleMouseOver = () => setHovered(true);
+  const handleMouseOut = () => setHovered(false);
 
-  const handleDateClick = (date) => {
-    setSelectedDate(date);
-  };
+  const buttonStyle = `min-w-[116px] h-[50px] p-0 pr-2 gap-2 border cursor-pointer rounded-lg transition-colors duration-300 relative ${isSelected || hovered ? "bg-[#7126B5] text-white" : "hover:bg-[#7126B5] hover:text-white"}`;
+
+  const dateStyle = `text-gray-600 font-poppins text-sm font-medium leading-4 text-center ${isSelected || hovered ? "text-white" : ""}`;
 
   return (
-    <div className="content max-w-full w-full mx-auto relative pt-1 flex gap-4 justify-center border-b border-gray-300 py-7 shadow-md">
-      {dummyDates.map((item, index) => (
-        <button
-          key={index}
-          onClick={() => handleDateClick(item.date)}
-          className={`flex flex-col items-center px-4 py-1 rounded-lg ${
-            selectedDate === item.date
-              ? "bg-[#A06ECE] text-white"
-              : "bg-white text-black"
-          }`}
-        >
-          <span
-            className={`font-medium ${
-              selectedDate === item.date ? "text-white" : "text-black"
-            }`}
-          >
-            {item.day}
-          </span>
-          <span
-            className={`text-sm ${
-              selectedDate === item.date ? "text-white" : "text-[#8A8A8A]"
-            }`}
-          >
-            {item.date}
-          </span>
-        </button>
-      ))}
+    <div>
+      <button onClick={() => onClick(date)} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseOut} className={buttonStyle}>
+        <h2 className="font-poppins text-lg font-semibold leading-5 text-center">{day}</h2>
+        <h3 className={dateStyle}>{date}</h3>
+        <div className="absolute right-0 top-24 w-24 h-0 border-l border-gray-300 opacity-0 transform rotate-90"></div>
+      </button>
     </div>
   );
-}
+};
 
 export default DateFilter;
