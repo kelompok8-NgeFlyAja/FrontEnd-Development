c91f5e9e9a048a0da52a1aec62e3ec6b891feb9b
AbdiPranawa <asaturaardana@gmail.com>
2024-12-08 17:34:26 +0700

    fix: cleaning code SoldOut

--- src/components/search/SoldOut.jsx
+++ src/components/search/SoldOut.jsx
@@ -1,16 +1,17 @@
-function SoldOut() {
-  return (
-    <div className="content max-w-full w-full mx-auto relative flex flex-col items-center py-7">
-      <img src="soldout.png" alt="soldout" className="w-1/3 mb-4" />
+import React from "react";
 
-      <div className="text-center">
-        <p className="text-lg font-bold mb-1">Maaf, tiket terjual habis!</p>
-        <p className="text-lg font-bold text-[#7126B5] pb-12">
-          Coba cari perjalanan lainnya!
-        </p>
+const SoldOut = () => {
+  return (
+    <div className="flex flex-col items-center gap-5 min-h-[50vh]">
+      <div>
+        <img src="soldout.png" alt="Tiket Habis" />
       </div>
+      <h1 className="text-center font-medium text-black">
+        <p>Maaf, tiket terjual habis!</p>
+        <span className="text-[#7126B5]"> Coba cari perjalanan lainnya!</span>
+      </h1>
     </div>
   );
-}
+};
 
 export default SoldOut;
