ebf0e2362c135b60d685bf04cf6e8ffd220c55e9
Brandon OddDuckk <Brandon.Ogaden@gmail.com>
2024-12-09 22:44:31 +0800

    fix: changed topnav to sticky to avoid overlap

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -95,7 +95,7 @@ const Topnav = ({ isLogin = false, isSearch, isOTP = false }) => {
   };
 
   return (
-    <nav className="bg-white shadow-md fixed top-0 left-0 w-full z-50 py-4 px-4 xl:px-28 flex justify-between items-center">
+    <nav className="bg-white shadow-md sticky top-0 left-0 w-full z-50 py-4 px-4 xl:px-28 flex justify-between items-center">
       <div className="flex flex-1 gap-10 items-center">
         <a href="/" onClick={() => navigate("/")}>
           <img
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,13 +2,34 @@ import Banner from "@/components/Banner";
 import FeauteredFlightCard from "@/components/homepage/FeauteredFlightCard";
 import SearchFeatured from "@/components/homepage/FeautredSearch";
 import FlightSearch from "@/components/homepage/FligthSearch";
+import Topnav from "@/components/TopNavbar";
 
 import Navbar from "@/components/Navbar";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 
 export default function Homepage() {
+  const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
+  const cookies = new Cookies();
+
+  useEffect(() => {
+    const checkToken = cookies.get("token");
+    if (checkToken) {
+      if (checkToken === "undefined") {
+        setIsLogin(false);
+      } else {
+        setIsLogin(true);
+      }
+    } else {
+      setIsLogin(false);
+    }
+  }, [navigate]);
+  
   return (
     <div>
-      <Navbar />
+      <Topnav isLogin={isLogin} isSearch={true} />
       <Banner />
       <FlightSearch />
       <SearchFeatured />
