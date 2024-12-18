2b09ed09ccd0a029deb27997fc08fef93f6a3ef3
Najmi Raihan <najmiraihan@Najmis-MacBook-Pro.local>
2024-12-18 16:58:54 +0700

    fix: review data and add some button

--- src/pages/checkout/ReviewData.jsx
+++ src/pages/checkout/ReviewData.jsx
@@ -1,16 +1,35 @@
 import React from "react";
-import { Card } from "@/components/ui/card";
+import { BillingDetail } from "@/components/checkoutC/BillingDetail";
+import SeatSelection from "@/components/checkoutC/SeatSelection";
+import { FlightDetail } from "@/components/checkoutC/FlightDetail";
 
 export const ReviewData = ({ handleProceedToPayment }) => {
   return (
-    <Card className="p-6">
-      <h2 className="font-semibold text-xl mb-4">Review Data</h2>
-      <button
-        onClick={handleProceedToPayment}
-        className="bg-yellow-500 text-white py-2 px-4 rounded-lg"
-      >
-        Lanjut Bayar
-      </button>
-    </Card>
+    <div className="flex justify-center items-center w-full py-8">
+      <div className="flex flex-col md:flex-row gap-8 max-w-[900px] w-full px-4">
+        {/* Isi Data Pemesan */}
+        <div className="flex-[2]">
+          <BillingDetail />
+          <SeatSelection />
+          <button
+            className="bg-[#D0D0D0] text-white py-3 mt-4 px-4 rounded-lg w-full"
+            disabled
+          >
+            Simpan
+          </button>
+        </div>
+
+        {/* Detail Penerbangan */}
+        <div className="flex-[1]">
+          <FlightDetail />
+          <button
+            onClick={handleProceedToPayment}
+            className="bg-[#FF0000] text-white py-3 mt-4 px-4 rounded-lg w-full"
+          >
+            Lanjut Bayar
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
